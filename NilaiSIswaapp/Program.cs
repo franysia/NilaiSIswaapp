b439cb2abc75215e79716028cad6f89543f9f6f2
franysia <francobocilff@gmail.com>
2026-01-12 09:50:32 +0700

    Input nilai & nama

--- NilaiSIswaapp/Program.cs
+++ NilaiSIswaapp/Program.cs
@@ -1,3 +1,12 @@
 ﻿int nilai;
  string nama;
 Console.WriteLine("AplikasiNilaiSiswa");
+
+Console.WriteLine("Input Nilai ");
+nama = Console.ReadLine();
+
+Console.WriteLine("Nama Siswa");
+nilai =  int.Parse(Console.ReadLine());
+
+
+    
